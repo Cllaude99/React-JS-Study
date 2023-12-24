54c27dbad1f69a1935e9e0233c5b0cfe5ac64f47
Cllaude99 <xodbsrla1025@naver.com>
2023-12-24 15:11:40 +0900

    02

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,5 +1,48 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotationAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${Emoji}:hover {
+    font-size: 60px;
+  }
+`;
+
 function App() {
-  return <p>sdfdsaaa</p>;
+  return (
+    <Wrapper>
+      <Box>
+        <Emoji>😊</Emoji>
+      </Box>
+    </Wrapper>
+  );
 }
 
 export default App;
