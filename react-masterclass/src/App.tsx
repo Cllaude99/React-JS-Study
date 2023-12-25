5d9649ea7f7b5cc2c7cf0c2385765c97120472a7
Cllaude99 <xodbsrla1025@naver.com>
2023-12-25 14:05:10 +0900

    03

---
+++ react-masterclass/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
