a899b20620ab9db589f6fef58038831220acce91
TaeYoon Kim <xodbsrla1025@naver.com>
2023-12-29 14:10:40 +0900

    06

--- react-masterclass/src/index.tsx
+++ react-masterclass/src/index.tsx
@@ -5,6 +5,7 @@ import { theme } from './theme';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Router';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
@@ -14,10 +15,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
