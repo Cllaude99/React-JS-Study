57d448a5d1b8b3022ffc5652f58546bdc591bca1
TaeYoon Kim <xodbsrla1025@naver.com>
2024-01-19 00:31:03 +0900

    08:13 "framer-motion 방향 관련 에러 문제 해결"

--- react-masterclass/src/pages/FramerMotion.tsx
+++ react-masterclass/src/pages/FramerMotion.tsx
@@ -1,35 +1,51 @@
 import styled from 'styled-components';
-import {
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useTransform,
-} from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
 
-const icon = {
-  hidden: {
-    pathLength: 0,
-    fill: 'rgba(255, 255, 255, 0)',
+const boxVariants = {
+  initial: (isNext: boolean) => ({
+    opacity: 0,
+    scale: 0,
+    x: isNext ? 500 : -500,
+  }),
+  animate: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
   },
-  visible: {
-    pathLength: 1,
-    fill: 'rgba(255, 255, 255, 1)',
-    transition: { duration: 3 },
-  },
+  exit: (isNext: boolean) => ({
+    opacity: 0,
+    scale: 0,
+    x: isNext ? -500 : 500,
+  }),
 };
 const FramerMotion = () => {
+  const [visible, setVisible] = useState(1);
+  const [isNext, setIsNext] = useState(true);
+  const nextPlease = async () => {
+    await setIsNext(true);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = async () => {
+    await setIsNext(false);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <motion.path
-          variants={icon}
-          initial="hidden"
-          animate="visible"
-          stroke="white"
-          strokeWidth="2"
-          d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"
-        />
-      </Svg>
+      <AnimatePresence initial={false}>
+        <Box
+          key={visible}
+          custom={isNext}
+          variants={boxVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 };
@@ -38,31 +54,23 @@ export default FramerMotion;
 
 const Wrapper = styled(motion.div)`
   display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
-const BiggerBox = styled(motion.div)`
+const Box = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  overflow: hidden;
-`;
-const Box = styled(motion.div)`
-  width: 200px;
+  font-size: 28px;
+  position: absolute;
+  top: 100px;
+  width: 400px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 25px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Svg = styled.svg`
-  width: 300px;
-  height: 100px;
 `;
