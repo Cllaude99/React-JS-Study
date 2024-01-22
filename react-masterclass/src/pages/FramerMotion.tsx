f158db51c6c8011c260a88167e4b111adece9d36
TaeYoon Kim <xodbsrla1025@naver.com>
2024-01-22 17:34:53 +0900

    08:END

--- react-masterclass/src/pages/FramerMotion.tsx
+++ react-masterclass/src/pages/FramerMotion.tsx
@@ -5,47 +5,46 @@ import { useState } from 'react';
 const boxVariants = {
   initial: (isNext: boolean) => ({
     opacity: 0,
-    scale: 0,
     x: isNext ? 500 : -500,
   }),
   animate: {
     opacity: 1,
-    scale: 1,
     x: 0,
   },
   exit: (isNext: boolean) => ({
     opacity: 0,
-    scale: 0,
     x: isNext ? -500 : 500,
   }),
 };
+
 const FramerMotion = () => {
-  const [visible, setVisible] = useState(1);
-  const [isNext, setIsNext] = useState(true);
-  const nextPlease = async () => {
-    await setIsNext(true);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevPlease = async () => {
+  const [value, setValue] = useState(1);
+  const [isNext, setIsNext] = useState(false);
+
+  const handlePrev = async () => {
     await setIsNext(false);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+    setValue((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+  const handleNext = async () => {
+    await setIsNext(true);
+    setValue((prev) => (prev === 10 ? 10 : prev + 1));
   };
   return (
     <Wrapper>
       <AnimatePresence initial={false}>
         <Box
-          key={visible}
-          custom={isNext}
+          key={value}
           variants={boxVariants}
+          custom={isNext}
           initial="initial"
           animate="animate"
           exit="exit"
         >
-          {visible}
+          {value}
         </Box>
       </AnimatePresence>
-      <button onClick={nextPlease}>next</button>
-      <button onClick={prevPlease}>prev</button>
+      <button onClick={handlePrev}>prev</button>
+      <button onClick={handleNext}>next</button>
     </Wrapper>
   );
 };
@@ -62,12 +61,12 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 const Box = styled(motion.div)`
+  position: absolute;
+  top: 30px;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 28px;
-  position: absolute;
-  top: 100px;
   width: 400px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
